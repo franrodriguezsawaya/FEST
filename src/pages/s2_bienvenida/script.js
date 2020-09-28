b6db5fc0350182cf61c6054e3ee9d26924d5bb7b
njmyers <17010801+njmyers@users.noreply.github.com>
2020-09-27 22:52:12 -0400

    Move to a src/pages style structure

---
+++ src/pages/s2_bienvenida/script.js
@@ -0,0 +1,17 @@
+let img;
+
+function preload() {
+  img = loadImage('/michelleobama.jpg');
+}
+function setup() {
+createCanvas(1000,600);
+background(255);
+image(img, 0, 0);
+
+}
+
+function draw(){
+
+
+
+}
